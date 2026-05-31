7ee7fc97c7c5fed0f4ad2c9209213b2baf0a0cfe
Taylor-Hallman <105246995+Taylor-Hallman@users.noreply.github.com>
2026-05-31 16:35:25 -0400

    improve shared ptr implementation

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
-#include "thread_pool/ThreadPool.h"
-
+#include <iostream>
+#include "shared_ptr/shared_ptr.h"
 int main() {
-
-    ThreadPool pool;
-    std::atomic<int> seen{0};
-
-    pool.submit([&] { seen.fetch_add(1, std::memory_order_relaxed); });
-
-    // Wait briefly for the task to run.
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-    const bool executed = seen.load(std::memory_order_relaxed) == 1;
-
-    return executed ? 0 : 1;
+    int* ptr = new int(1);
+    getcracked::shared_ptr<int> sh_ptr(ptr);
+    std::cout << sh_ptr.get_count() << std::endl;
+    getcracked::shared_ptr<int> sh_ptr2(std::move(sh_ptr));
+    std::cout << sh_ptr.get_count() << " " << sh_ptr2.get_count() << std::endl;
 }
--- shared_ptr/shared_ptr.h
+++ shared_ptr/shared_ptr.h
@@ -1,4 +1,9 @@
-#pragma once
+// Write your solution here
+// C++23 using GCC 14.2
+// Debug with std::cerr or std::clog.
+// !!! IMPORTANT !!!
+// 99% of headers are pre-compiled for you server-side.
+// If your submission fails to compile due to a missing header, add it to your submission.
 
 #include <cstddef>
 #include <mutex>
@@ -9,20 +14,6 @@ namespace getcracked
     {
         size_t count_{ };
         mutable std::mutex mutex_;
-
-        control_block() : count_(0) {}
-
-        control_block(int count) : count_(count) {}
-
-        void inc() {
-            std::lock_guard<std::mutex> lock(mutex_);
-            count_++;
-        }
-
-        void dec() {
-            std::lock_guard<std::mutex> lock(mutex_);
-            if (count_ > 0) count_--;
-        }
     };
 
     template <typename T>
@@ -30,79 +21,117 @@ namespace getcracked
     {
     public:
 
-        shared_ptr() : m_Ptr(new T()), m_ControlBlock(new control_block(1)) {}
-
-        shared_ptr(T* pointer) : m_Ptr(pointer), m_ControlBlock(new control_block(1))
+        shared_ptr() : m_Ptr{ nullptr }, m_Cb{ new control_block() } { }
+        shared_ptr(T* pointer) : m_Ptr{ pointer }, m_Cb(new control_block{ 1 })
         {
-
         }
 
-        shared_ptr(const shared_ptr& other) noexcept : m_Ptr(other.m_Ptr), m_ControlBlock(other.m_ControlBlock)
+        shared_ptr(const shared_ptr& other) noexcept : m_Ptr(other.m_Ptr), m_Cb(other.m_Cb)
         {
-            m_ControlBlock->inc();
+            std::scoped_lock lock(m_Cb->mutex_);
+            ++m_Cb->count_;
         }
 
         shared_ptr& operator=(const shared_ptr& other) noexcept
         {
-            if (&other == this)
+            if (this == &other)
                 return *this;
-            m_ControlBlock->dec();
+            if (m_Ptr) {
+                delete m_Ptr;
+                std::scoped_lock lock(m_Cb->mutex_);
+                --m_Cb->count_;
+            }
+            else
+                delete m_Cb;
             m_Ptr = other.m_Ptr;
-            m_ControlBlock = other.m_ControlBlock;
-            m_ControlBlock->inc();
+            m_Cb = other.m_Cb;
+            {
+                std::scoped_lock lock(m_Cb->mutex_);
+                ++m_Cb->count_;
+            }
             return *this;
         }
 
-        shared_ptr(shared_ptr&& other) noexcept : m_Ptr(new T(std::move(*other.m_Ptr))), m_ControlBlock(other.m_ControlBlock)
+        shared_ptr(shared_ptr&& other) noexcept : m_Ptr(other.m_Ptr), m_Cb(other.m_Cb)
         {
             other.m_Ptr = nullptr;
-            other.m_ControlBlock = nullptr;
+            other.m_Cb = nullptr;
         }
 
         shared_ptr& operator=(shared_ptr&& other) noexcept
         {
-            if (&other == this)
+            if (this == &other)
                 return *this;
-            m_ControlBlock->dec();
-            m_Ptr = new T(std::move(*other.m_Ptr));
-            m_ControlBlock = other.m_ControlBlock;
+            if (m_Ptr) {
+                delete m_Ptr;
+                std::scoped_lock lock(m_Cb->mutex_);
+                --m_Cb->count_;
+                if (!m_Cb->count_) {
+                    delete m_Ptr;
+                    delete m_Cb;
+                }
+            }
+            else
+                delete m_Cb;
+            m_Ptr = other.m_Ptr;
+            m_Cb = other.m_Cb;
             other.m_Ptr = nullptr;
-            other.m_ControlBlock = nullptr;
+            other.m_Cb = nullptr;
             return *this;
         }
 
         ~shared_ptr()
         {
-            if (!m_ControlBlock) {
-                delete m_Ptr;
+            if (!m_Cb)
+                return;
+            if (!m_Cb->count_) {
+                delete m_Cb;
                 return;
             }
-            m_ControlBlock->dec();
-            if (m_ControlBlock->count_ == 0) {
+            {
+               std::scoped_lock lock(m_Cb->mutex_);
+                --m_Cb->count_;
+            }
+            if (!m_Cb->count_) {
                 delete m_Ptr;
-                delete m_ControlBlock;
+                delete m_Cb;
             }
         }
 
         void reset(T* pointer)
         {
-            this->~shared_ptr();
+            if (m_Cb->count_) {
+                std::scoped_lock lock(m_Cb->mutex_);
+                --m_Cb->count_;
+            }
+            if (!m_Cb->count_) {
+                delete m_Ptr;
+                delete m_Cb;
+            }
             m_Ptr = pointer;
-            m_ControlBlock = new control_block();
+            m_Cb = new control_block();
         }
 
 
         size_t get_count() const
         {
-            return m_ControlBlock ? m_ControlBlock->count_ : 0;
+            if (!m_Cb)
+                return 0;
+            return m_Cb->count_;
         }
 
-        T* operator->() const { return m_Ptr; }
-        T& operator*() const { return *m_Ptr; }
-        operator bool() const noexcept { return m_Ptr != nullptr; }
+        T* operator->() const { 
+            return m_Ptr;
+        }
+        T& operator*() const { 
+            return *m_Ptr;
+        }
+        operator bool() const noexcept { 
+            return m_Ptr != nullptr;
+        }
 
     private:
-        T* m_Ptr = nullptr;
-        control_block* m_ControlBlock;
+        T* m_Ptr;
+        control_block* m_Cb;
     };
 }
