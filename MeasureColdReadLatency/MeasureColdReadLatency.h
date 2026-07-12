d54d448e391c10aea1e3a8e91a39a6904866fd1c
Taylor-Hallman <105246995+Taylor-Hallman@users.noreply.github.com>
2026-07-12 16:31:34 -0400

    Redo measuring latency of cold read of l1 cache

--- MeasureColdReadLatency/MeasureColdReadLatency.h
+++ MeasureColdReadLatency/MeasureColdReadLatency.h
@@ -1,65 +1,36 @@
+#include <new>
+#include <vector>
+#include <cstdint>
 #include <chrono>
-#include <thread>
-#include <cassert>
-#include <immintrin.h>
 
-static constexpr size_t num_trials{ 100uz };
-static constexpr int64_t calibration_window{ 100 };
+constexpr auto CacheLineSize = std::hardware_destructive_interference_size;
 
-double calibrate() {
-    auto cycles_start{ __rdtsc() };
-    auto time_start{ std::chrono::high_resolution_clock::now() };
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(calibration_window));
-
-    auto cycles_end{ __rdtsc() };
-    auto time_end{ std::chrono::high_resolution_clock::now() };
-
-    auto elapsed_ns{ static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(time_end - time_start ).count()) };
-    return static_cast<double>(cycles_end - cycles_start) / elapsed_ns;
-}
-
-template<typename RandomAccessItr>
-double median(RandomAccessItr begin, RandomAccessItr end) {
-    auto size{ std::distance(begin, end) };
-    auto mid{ size / 2 };
-    assert(size > 0);
-
-    std::nth_element(begin, begin + mid, end);
-    auto upper{ *(begin + mid) };
-    if (size % 2)
-        return upper;
-    
-    std::nth_element(begin, begin + mid - 1, end);
-    return (upper + *(begin + mid - 1)) / 2.0;
-}
+struct alignas(CacheLineSize) CacheLinePadded {
+    uint8_t m_data = 0;
+};
 
 double MeasureL1ColdReadLatency() {
-    std::vector<double> measurements(num_trials);
+    constexpr auto CacheFloodSize = CacheLineSize * (2 << 12); // 500k bytes 
+    constexpr auto Trials = 20'000;
+    CacheLinePadded data;
+    std::vector<uint8_t> Vec(CacheFloodSize);
+    double FloodTime = 0.0;
+    volatile uint64_t sink = 0;
 
-    double cycles_per_ns{ calibrate() };
+    using namespace std::chrono;
+    auto TimeStart = steady_clock::now();
 
-    const int* ptr = new int(0);
-
-    for (size_t i{}; i < num_trials; ++i) {
-        _mm_clflush(static_cast<const void*>(ptr)); // flush the cache line
-        _mm_mfence(); // wait for the flush to finish
-
-        _mm_lfence();
-        auto cycles_start{ __rdtsc() };
-        
-        // perform the cold read
-        [[maybe_unused]] int val = *(volatile int*)ptr;
-
-        _mm_lfence();
-        auto cycles_end{ __rdtsc() };
-
-        auto cycles{ cycles_end - cycles_start };
-        measurements[i] = static_cast<double>(cycles) / cycles_per_ns;
+    for (size_t trials{}; trials < Trials; ++trials) {
+        auto FloodStart = steady_clock::now();
+        for (size_t i{}; i < CacheFloodSize; i += CacheLineSize) {
+            sink += Vec[i];
+        }
+        auto FloodEnd = steady_clock::now();
+        sink += data.m_data;
+        FloodTime += static_cast<double>(duration_cast<nanoseconds>(FloodEnd - FloodStart).count());
     }
 
-    delete ptr;
-
-    // return the median rather than the mean so extremes don't throw off the measurement
-    return median(measurements.begin(), measurements.end());
+    auto TimeEnd = steady_clock::now();
+    double TotalTime = static_cast<double>(duration_cast<nanoseconds>(TimeEnd - TimeStart).count());
+    return (TotalTime - FloodTime) / Trials;
 }
--- main.cpp
+++ main.cpp
@@ -1,27 +1,7 @@
-#include "optional/optional.h"
-#include <cassert>
-#include <print>
-
-class Foo {
-private:
-    char mem;
-public:
-    Foo(int num) : mem(num) {}
-
-    ~Foo() {
-        std::println("Calling non-trivial destructor");
-    }
-};
-
-void fun() {
-    aux::optional<Foo> opt;
-    assert(!opt.has_value());
-    aux::optional<Foo> opt2 = Foo(5);
-    assert(opt2.has_value());
-    aux::optional<int> opt3(5);
-    std::println("{}", opt3.value());
-}
+#include "MeasureColdReadLatency/MeasureColdReadLatency.h"
+#include <iostream>
 
 int main() {
-    fun();
+    double d = MeasureL1ColdReadLatency();
+    std::cout << d << "\n";
 }
